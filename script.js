4af261f8cf0287d199b4f10f08884e6edec30a08
younasBnHasainar <unusbnhassainar@gmail.com>
2023-08-13 23:13:12 +0530

    layout has set

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+  const outerGrid = document.querySelector(".outer-grid")
+  outerGrid.classList.toggle("outer-grid-expanded")
+  const nav = document.querySelector("nav")
+  nav.classList.toggle("hide-nav")
+}
